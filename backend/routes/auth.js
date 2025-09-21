a327b7023811988ada15f74bb99a21fe03cc2709
Harshvardhan-patil04 <154110417+Harshvardhan-patil04@users.noreply.github.com>
2025-09-21 09:33:53 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-// Signup
+
 router.post("/signup", async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -17,7 +17,6 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-// Login (just for test, Basic Auth used on other routes)
 router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
